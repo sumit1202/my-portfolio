62bde60f2a8115de1678b9b7cbb2afe06e899ecc
sumitkumar <sumitkumar1202@gmail.com>
2023-12-31 11:50:40 +0530

    added confetti to light/dark toggle button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import confetti from "https://cdn.skypack.dev/canvas-confetti";
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
@@ -41,6 +43,8 @@ function setDarkMode() {
   themeIcons.forEach((icon) => {
     icon.src = icon.getAttribute("src-dark");
   });
+
+  makeConfetti();
 }
 
 function setLightMode() {
@@ -49,5 +53,62 @@ function setLightMode() {
 
   themeIcons.forEach((icon) => {
     icon.src = icon.getAttribute("src-light");
+  });
+
+  makeConfetti();
+}
+
+
+function makeConfetti(){
+  makeConfettiDefault();
+}
+
+// confetti options
+
+function makeConfettiDefault() {
+  confetti({
+    particleCount: 150,
+    spread: 500,
+  });
+}
+
+function makeConfettiForCustomTime() {
+  // do this for 30 seconds
+  var duration = 5 * 1000;
+  var end = Date.now() + duration;
+
+  (function frame() {
+    // launch a few confetti from the left edge
+    confetti({
+      particleCount: 7,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+    });
+    // and launch a few from the right edge
+    confetti({
+      particleCount: 7,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+    });
+
+    // keep going until we are out of time
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+}
+
+function makeConfettiRandom() {
+  confetti({
+    particleCount: 100,
+    startVelocity: 30,
+    spread: 360,
+    origin: {
+      x: Math.random(),
+      // since they fall down, start a bit higher than random
+      y: Math.random() - 0.2,
+    },
   });
 }
